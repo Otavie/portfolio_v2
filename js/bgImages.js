cc7228d564054252cc8ad45344a8ef033256780d
Otavie <otavie.okuoyopgs@stu.cu.edu.ng>
2022-10-09 22:53:36 +0100

    Reduction of Image Sizes

--- js/bgImages.js
+++ js/bgImages.js
@@ -1,15 +1,15 @@
 const landing = document.querySelector('.landing');
 imgV = [
-    'https://otavie.github.io/portfolio_v2/img/v1.png',
-    'https://otavie.github.io/portfolio_v2/img/v2.png',
-    'https://otavie.github.io/portfolio_v2/img/v3.png',
-    'https://otavie.github.io/portfolio_v2/img/v4.png'
+    'https://otavie.github.io/portfolio_v2/img/v1.jpg',
+    'https://otavie.github.io/portfolio_v2/img/v2.jpg',
+    'https://otavie.github.io/portfolio_v2/img/v3.jpg',
+    'https://otavie.github.io/portfolio_v2/img/v4.jpg'
 ];
 imgH = [
-    '../img/h1.png', 
-    '../img/h2.png', 
-    '../img/h3.jpg', 
-    '../img/h4.jpg'
+    'https://otavie.github.io/portfolio_v2/img/h1.jpg',
+    'https://otavie.github.io/portfolio_v2/img/h2.jpg',
+    'https://otavie.github.io/portfolio_v2/img/h3.jpg',
+    'https://otavie.github.io/portfolio_v2/img/h4.jpg'
 ];
 
 
