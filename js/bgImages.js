faf81e8175abbdd7811943eeb8bc83e36d8c44aa
Otavie <otavie.okuoyopgs@stu.cu.edu.ng>
2022-10-09 22:19:11 +0100

    Reduction of Imgs for Mobile Display

--- js/bgImages.js
+++ js/bgImages.js
@@ -4,37 +4,42 @@ imgV = [
     'https://otavie.github.io/portfolio_v2/img/v2.png',
     'https://otavie.github.io/portfolio_v2/img/v3.png',
     'https://otavie.github.io/portfolio_v2/img/v4.png'
-    // '../img/v1.png', 
-    // '../img/v2.png', 
-    // '../img/v3.png', 
-    // '../img/v4.png'
 ];
 imgH = [
     'https://otavie.github.io/portfolio_v2/img/h1.png',
     'https://otavie.github.io/portfolio_v2/img/h2.png',
     'https://otavie.github.io/portfolio_v2/img/h3.png',
     'https://otavie.github.io/portfolio_v2/img/h4.png'
-    // '../img/h1.png', 
-    // '../img/h2.png', 
-    // '../img/h3.jpg', 
-    // '../img/h4.jpg'
 ];
 
+
+function imagesH (){
+    const i = Math.floor(Math.random() * imgH.length);
+    landing.style.backgroundImage = 'url('+imgH[i]+')';
+}
+
+function imagesV (){
+    const j = Math.floor(Math.random() * imgV.length);
+    landing.style.backgroundImage = 'url('+imgH[j]+')';
+}
+
 var mediaQuery = window.matchMedia("(min-width: 600px)");
-function bgChanger(){
+
+function changeBg(){
     if (mediaQuery.matches){
-        setInterval(function(){
-            const i = Math.floor(Math.random() * imgH.length);
-            landing.style.backgroundImage = 'url('+imgH[i]+')';
-        }, 4000);
-        clearInterval();
-    }else{
-        setInterval(function(){
-            const j = Math.floor(Math.random() * imgH.length);
-            landing.style.backgroundImage = 'url('+imgV[j]+')';
-        }, 4000);
-        clearInterval();
+        clearInterval(imagesV);
+        setInterval(imagesH, 4000);
+        
     }
 }
 
-window.onload = bgChanger();
+function changeBg_T(){
+    if (!(mediaQuery.matches)){
+        clearInterval(imagesH);
+        setInterval(imagesV, 4000);
+        
+    }
+}
+
+window.onload = changeBg();
+window.onload = changeBg_T();
