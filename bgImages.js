3bec92ffe785f2c28d914404d392aa4784319e9f
Otavie <otavie.okuoyopgs@stu.cu.edu.ng>
2022-10-10 13:39:49 +0100

    Change Directory of the CSS and the Random Image Gen Files

--- bgImages.js
+++ bgImages.js
@@ -1,15 +1,17 @@
 const landing = document.querySelector('.landing');
 imgV = [
-    'https://otavie.github.io/portfolio_v2/img/v1.jpg',
-    'https://otavie.github.io/portfolio_v2/img/v2.jpg',
-    'https://otavie.github.io/portfolio_v2/img/v3.jpg',
-    'https://otavie.github.io/portfolio_v2/img/v4.jpg'
+    // 'https://otavie.github.io/portfolio_v2/img/v1.jpg'
+    'img/v1.jpg',
+    'img/v2.jpg',
+    'img/v3.jpg',
+    'img/v4.jpg'
 ];
 imgH = [
-    'https://otavie.github.io/portfolio_v2/img/h1.jpg',
-    'https://otavie.github.io/portfolio_v2/img/h2.jpg',
-    'https://otavie.github.io/portfolio_v2/img/h3.jpg',
-    'https://otavie.github.io/portfolio_v2/img/h4.jpg'
+    // 'https://otavie.github.io/portfolio_v2/img/h1.jpg'
+    'img/h1.jpg',
+    'img/h2.jpg',
+    'img/h3.jpg',
+    'img/h4.jpg'
 ];
 
 function imagesH (){
